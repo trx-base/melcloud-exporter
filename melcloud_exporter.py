69535fb0204c9d02c992d7cd0a62390eb15ee416
Etjen Delilovic <etjen.delilovic@trx-base.com>
2024-08-23 13:36:32 +0200

    Changed implementation to generic data extraction from MELCloud devices.

--- melcloud_exporter.py
+++ melcloud_exporter.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-__author__ = "Oliver Schlueter"
-__license__ = "GPL"
-__version__ = "1.0.0"
-__email__ = "oliver.schlueter@dell.com"
-__status__ = "Production"
-
 """"
 ###########################################################################################################
   Prometheus Exporter for Mitsubishi MelCloud Devices
@@ -32,8 +26,7 @@ import time
 import json
 import requests
 import datetime
-from prometheus_client import start_http_server, Gauge, Info, Enum
-
+from prometheus_client import start_http_server, Gauge
 
 class MelCloudMetrics:
     headers = {
@@ -52,24 +45,19 @@ class MelCloudMetrics:
             "Password": self.mel_cloud_password,
             "AppVersion": "1.23.4.0"
         }
-        # Prometheus' metrics to collect
-        self.device_name = Info("device_name", "Device Name")
-        self.power = Enum('power_state', 'Power Status', ['room'], states=['on', 'off'])
-        self.total_energy_consumed = Gauge("total_energy_consumed", "Total Energy Consumed", ['room'])
-        self.wifi_signal = Gauge("wifi_signal", "Wifi Signal", ['room'])
-        self.room_temperature = Gauge("room_temperature", "Room Temperature", ['room'])
-        self.target_temperature = Gauge("target_temperature", "Target Temperature", ['room'])
-        self.operation_mode = Enum('operation_mode', 'Operation Mode', ['room'],
-                                   states=["heat", "dry", "cool", "fan_only", "heat_cool", "undefined"])
-        self.fan_speed = Gauge('fan_speed', 'Fan Speed', ['room'])
-        self.vane_horizontal = Gauge('vane_horizontal', 'Vane Horizontal', ['room'])
-        self.vane_vertical = Gauge('vane_vertical', 'Vane Vertical', ['room'])
+        self.metrics = {}
+
+    def create_or_get_metric(self, name, description, room):
+        if name not in self.metrics:
+            self.metrics[name] = Gauge(name, description, ['room'])
+
+        return self.metrics[name].labels(room)
 
     def retrieve_mel_cloud_data(self):
+        global devices
         error = False
         timestamp = datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S)")
         try:
-            # try to get token
             url = 'https://app.melcloud.com/Mitsubishi.Wifi.Client/Login/ClientLogin'
             response = requests.post(url, headers=self.headers, data=json.dumps(self.data))
             out = json.loads(response.text)
@@ -85,11 +73,11 @@ class MelCloudMetrics:
 
         if not error:
             try:
-                # try to get device data
                 url = 'https://app.melcloud.com/Mitsubishi.Wifi.Client/User/Listdevices'
                 response = requests.get(url, headers=self.headers, data=json.dumps(self.data))
                 out = json.loads(response.text)
                 devices = out[0]['Structure']['Devices']
+                print("Retrieved devices: " + str(len(devices)) )
             except Exception as err:
                 print(timestamp + ": Not able to get device data: " + str(err))
                 error = True
@@ -98,47 +86,22 @@ class MelCloudMetrics:
                 try:
                     for device in devices:
                         room = device['DeviceName']
+                        for key, value in device['Device'].items():
+                           if isinstance(value, (int, float, bool)):
+                                metric = self.create_or_get_metric(key, key, room)
+                                metric.set(float(value))
 
-                        self.device_name.info({"device_name": room})
-
-                        print(device['Device']['Power'])
-                        if device['Device']['Power']:
-                            self.power.labels(room).state("on")
-                        else:
-                            self.power.labels(room).state("off")
-
-                        self.total_energy_consumed.labels(room).set(device['Device']['CurrentEnergyConsumed'])
-                        self.wifi_signal.labels(room).set(device['Device']['WifiSignalStrength'])
-
-                        self.room_temperature.labels(room).set(device['Device']['RoomTemperature'])
-                        self.target_temperature.labels(room).set(device['Device']['SetTemperature'])
-
-                        # 1: Heating, 2: Drying, 3: Cooling, 7: Van, 8: Auto
-                        if device['Device']['OperationMode'] == 1:
-                            self.operation_mode.labels(room).state("heat")
-                        if device['Device']['OperationMode'] == 2:
-                            self.operation_mode.labels(room).state("dry")
-                        if device['Device']['OperationMode'] == 3:
-                            self.operation_mode.labels(room).state("cool")
-                        if device['Device']['OperationMode'] == 7:
-                            self.operation_mode.labels(room).state("fan_only")
-                        if device['Device']['OperationMode'] == 8:
-                            self.operation_mode.labels(room).state("heat_cool")
-
-                        self.fan_speed.labels(room).set(device['Device']['FanSpeed'])
-                        self.vane_horizontal.labels(room).set(device['Device']['VaneVerticalDirection'])
-                        self.vane_vertical.labels(room).set(device['Device']['VaneHorizontalSwing'])
                 except Exception as err:
-                    print(timestamp + ": Not able to get read values: " + str(err))
+                    print(timestamp + ": Not able to read values: " + str(err))
                     error = True
+
     def run_metrics_loop(self):
         while True:
+            print("Polling MELCloud API with interval of " + str(self.polling_interval_seconds) + " seconds.")
             self.retrieve_mel_cloud_data()
             time.sleep(self.polling_interval_seconds)
 
-
 def main():
-    """Main entry point"""
     try:
         polling_interval_seconds = int(os.environ['MEL_CLOUD_PORT_INTERVAL'])
     except:
@@ -163,6 +126,5 @@ def main():
     start_http_server(port)
     app_metrics.run_metrics_loop()
 
-    
 if __name__ == "__main__":
     main()
